e7dda7b7f0063ff824e9c75b32bde31903e984e1
Jeff Lai <jeff.lai@may4s.com.tw>
2025-06-10 17:18:59 +0800

    update

--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 // 測試發送 Every8d SMS 的功能
-
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -23,49 +22,19 @@ async function getToken() {
   );
   return res?.data?.data?.token || null;
 }
-// let tokenCache = {
-//   token: null,
-//   timestamp: null,
-// };
-// async function getToken() {
-//   const now = Date.now();
-//   const validFor = 10 * 60 * 1000; // 10 分鐘
 
-//   // 若 token 尚有效，直接使用快取
-//   if (tokenCache.token && now - tokenCache.timestamp < validFor) {
-//     console.log('get token from cache');
-//     return tokenCache.token;
-//   }
-
-//   // 否則重新取得
-//   const res = await axios.post(
-//     `${SiteUrl}/${custcode}/getoken`,
-//     { uid, pwd },
-//     { headers: { 'Content-Type': 'application/json' } }
-//   );
-//   const newToken = res?.data?.data?.token;
-//   console.log('get new token');
-
-//   // 更新快取
-//   if (newToken) {
-//     tokenCache.token = newToken;
-//     tokenCache.timestamp = now;
-//   }
-
-//   return newToken;
-// }
-
-async function sendSMS(token, sendtime = '') {
+// 一般簡訊
+async function sendSMS(token, sendtime) {
   const res = await axios.post(
     `${SiteUrl}/${custcode}/sendsms`,
     {
       uid, // must be set
-      // pwd,
-      token, // must be set
-      subject: '測試主旨',
+      pwd,
+      // token, // must be set
+      // subject: '測試主旨',
       msg: '安安你好！這是 evry8d 測試簡訊 from Jeff Lai', // must be set
-      mobiles: '+886975031751', // must be set
-      sendtime,
+      mobiles: process.env.mobile, // must be set
+      sendtime, // 預設為空字串，立即發送；給值則為排程發送，格式為 yyyyMMddHHmmss
       retrytime: '1440',
     },
     {
@@ -77,21 +46,25 @@ async function sendSMS(token, sendtime = '') {
   return batchid;
 }
 
-async function sendParamSMS(token) {
+// 參數簡訊
+async function sendParamSMS(token, sendtime) {
   const res = await axios.post(
     `${SiteUrl}/${custcode}/sendparam_sms`,
     {
       uid,
-      token,
-      subject: '參數簡訊測試',
-      paramsContent: '親愛的%field1%您好，您的訂單編號為%field2%',
+      pwd,
+      // token,
+      // subject: '參數簡訊測試',
+      paramsContent: '親愛的%field1%您好，您的訂單編號為%field2%', // 參數依序代入 %field%
+      // paramsContent: '測試沒有參數的參數簡訊',
+      // 發送內容沒有 %field% 變數時，則與一般簡訊相同，此時需完全移除參數 (非設定為空字串)
       retrytime: '1440',
       recipientdatalist: [
         {
-          mobile: '+886975031751',
-          sendtime: '',
-          params: '阿儒,ABC123456',
-          mr: 'test-01',
+          mobile: process.env.mobile,
+          sendtime, // 預設為空字串，立即發送；給值則為排程發送，格式為 yyyyMMddHHmmss
+          params: '小明,ABC123456', // 最多代入 5 個參數，
+          // mr: 'test-01',
         },
       ],
     },
@@ -103,6 +76,7 @@ async function sendParamSMS(token) {
   return res?.data?.data?.batchid;
 }
 
+// 取消排程發送的簡訊
 async function cancelSchedule(token, batchid) {
   const res = await axios.post(
     `${SiteUrl}/${custcode}/cancelbooking`,
@@ -119,6 +93,7 @@ async function cancelSchedule(token, batchid) {
   console.log('取消排程結果：', res.data);
 }
 
+// 查詢簡訊發送狀態
 async function getSMSStatus(token, batchid) {
   const res = await axios.post(
     `${SiteUrl}/${custcode}/getdrstatus`,
@@ -127,7 +102,7 @@ async function getSMSStatus(token, batchid) {
       token,
       batchid,
       type: 'SMS',
-      pageno: '1',
+      // pageno: '1',
     },
     {
       headers: { 'Content-Type': 'application/json' },
@@ -139,7 +114,8 @@ async function getSMSStatus(token, batchid) {
 
 // CLI 執行區塊
 const action = process.argv[2];
-const param = process.argv[3];
+const sendtime = process.argv[3] || ''; // 預設為空字串，立即發送；給值則為排程發送，格式為 yyyyMMddHHmmss
+const bid = process.argv[4];
 
 (async () => {
   try {
@@ -148,27 +124,50 @@ const param = process.argv[3];
     if (!token) throw new Error('無法取得 token');
 
     // 2. 發送簡訊或查詢狀態
+    // 2.1 一般簡訊
     if (action === 'send') {
       const batchid = await sendSMS(token);
-      console.log('Batch ID:', batchid);
-    } else if (action === 'status') {
-      if (!param) throw new Error('請提供 batchid');
-      await getSMSStatus(token, param);
-    } else if (action === 'schedule') {
-      const scheduleTime = '20250529173000'; // yyyyMMddHHmmss
-      const batchid = await sendSMS(token, scheduleTime);
-      console.log('排程發送 batchid：', batchid);
-    } else if (action === 'param') {
-      const batchid = await sendParamSMS(token);
-      console.log('參數簡訊 batchid：', batchid);
-    } else if (action === 'cancel') {
+      console.log('batchid: ', batchid);
+      return;
+    }
+
+    // 2.2 排程一般簡訊
+    if (action === 'schedule') {
+      if (!sendtime) throw new Error('請輸入 sendtime, 格式為 yyyyMMddHHmmss');
+      const batchid = await sendSMS(token, sendtime);
+      console.log('排程發送 batchid: ', batchid);
+      return;
+    }
+
+    // 2.3 參數簡訊
+    if (action === 'param') {
+      const batchid = await sendParamSMS(token, sendtime);
+      console.log('參數簡訊 batchid: ', batchid);
+      return;
+    }
+
+    // 2.4 取消排程發送的簡訊
+    if (action === 'cancel') {
       if (!param) throw new Error('請提供要取消的 batchid');
       await cancelSchedule(token, param);
-    } else {
-      console.log(
-        '請指定動作：send、status [batchid]、schedule、cancel [batchid]、param'
-      );
+      return;
     }
+
+    // 2.5 查詢簡訊發送狀態
+    if (action === 'status') {
+      if (!bid) throw new Error('請提供 batchid');
+      await getSMSStatus(token, bid);
+      return;
+    }
+
+    console.log(`
+      請執行想測試的項目:
+        send /
+        schedule sendtime /
+        param (sendtime)/
+        status [batchid] /
+        cancel [batchid] /
+      `);
   } catch (err) {
     console.error('執行時發生錯誤：', err.message);
   }
